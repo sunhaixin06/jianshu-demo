9db0010480e6275b69d3034e5ca4545f0661a0ef
sunhaixin <1113285307.com>
2018-11-11 10:50:45 +0800

    添加注释

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,17 @@ import reducer from './reducer';
 import thunk from 'redux-thunk';
 
 const composeEnhancers =  
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //redux-thunk 配置redux-devtools
 
+// Store 就是保存数据的地方， 你可以把它看成一个容器, 整个应用只能有一个 Store
+// Redux 提供 createStore 这个函数, 用来生成 Store
 const store = createStore(
 	reducer, 
-	composeEnhancers(applyMiddleware(thunk)),
+	composeEnhancers(applyMiddleware(thunk)), // action 创建函数
 );
-
+// createStore 函数接受另一个函数作为参数，返回新生成的 Store 对象
 export default store;
 
-
+// store.getState() 获取当前值
+// store.subscribe(() => {}) 创建一个监听
+// store.dispatch(action) 分发任务
